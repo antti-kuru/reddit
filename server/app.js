8e36e6bfdcffe02d3ec185a7de0e292245f3cf9c
Antti Kuru <Antti.Kuru@aalto.fi>
2025-11-17 19:56:03 +0200

    Updated app.js to use commentController

--- server/app.js
+++ server/app.js
@@ -1,15 +1,12 @@
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
-// importing database client
-import postgres from "postgres";
 
 import * as communityController from "./controllers/communityController.js"
-
+import * as postController from "./controllers/postController.js"
+import * as commentController from "./controllers/commentController.js"
 
 const app = new Hono();
-// creating an instance of the database client
-const sql = postgres();
 
 app.use("/*", cors());
 app.use("/*", logger());
@@ -25,6 +22,27 @@ app.get("/api/communities/:communityId", communityController.readOne)
 
 // DELETE community
 app.delete("/api/communities/:communityId", communityController.deleteOne)
+
+// POST post
+app.post("/api/communities/:communityId/posts", postController.create)
+
+// GET posts
+app.get("/api/communities/:communityId/posts", postController.readAll)
+
+// GET post
+app.get("/api/communities/:communityId/posts/:postId", postController.readOne)
+
+// DELETE post
+app.delete("/api/communities/:communityId/posts/:postId", postController.deleteOne)
+
+// POST comment
+app.post("/api/communities/:communityId/posts/:postId/comments", commentController.create)
+
+// GET comments
+app.get("/api/communities/:communityId/posts/:postId/comments", commentController.readAll)
+
+// DELETE comment
+app.delete("/api/communities/:communityId/posts/:postId/comments/:commentId", commentController.deleteOne)
 
 
 export default app;
