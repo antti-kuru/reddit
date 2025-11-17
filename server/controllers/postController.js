35c200b5da42a5dfcc02813b35176e6f78a4f1d0
Antti Kuru <Antti.Kuru@aalto.fi>
2025-11-17 19:17:27 +0200

    Added controller for posts

---
+++ server/controllers/postController.js
@@ -0,0 +1,66 @@
+import * as postRepository from "../repositories/postRepository.js"
+
+const create = async (c) => {
+    const communityId = Number(c.req.param('communityId'))
+    if (!Number.isInteger(communityId)) {
+        return c.json({ error: "Invalid community id" }, 400)
+    }
+
+    const post = await c.req.json()
+    if (!post.content) {
+        return c.json({ error: "Missing required fields" }, 400)
+    }
+
+    const newPost = await postRepository.create(communityId, post)
+    return c.json(newPost, 201)
+}
+
+const readAll = async (c) => {
+    const communityId = Number(c.req.param('communityId'))
+    if (!Number.isInteger(communityId)) {
+        return c.json({ error: "Invalid community id" }, 400)
+    }
+
+    const posts = await postRepository.findAll(communityId)
+    return c.json(posts)
+}
+
+const readOne = async (c) => {
+    const communityId = Number(c.req.param('communityId'))
+    const postId = Number(c.req.param('postId'))
+
+    if (!Number.isInteger(communityId)) {
+        return c.json({ error: "Invalid community id" }, 400)
+    }
+    if (!Number.isInteger(postId)) {
+        return c.json({ error: "Invalid post id" }, 400)
+    }
+
+    const post = await postRepository.findById(communityId, postId)
+    if (!post) {
+        return c.json({ error: "Post not found" }, 404)
+    }
+
+    return c.json(post)
+}
+
+const deleteOne = async (c) => {
+    const communityId = Number(c.req.param('communityId'))
+    const postId = Number(c.req.param('postId'))
+
+    if (!Number.isInteger(communityId)) {
+        return c.json({ error: "Invalid community id" }, 400)
+    }
+    if (!Number.isInteger(postId)) {
+        return c.json({ error: "Invalid post id" }, 400)
+    }
+
+    const deletedPost = await postRepository.deleteById(communityId, postId)
+    if (!deletedPost) {
+        return c.json({ error: "Post not found" }, 404)
+    }
+
+    return c.json(deletedPost)
+}
+
+export { create, readAll, readOne, deleteOne }
