d642634fd24ca9d7fb48c114e3a5fd5714efa945
Antti Kuru <Antti.Kuru@aalto.fi>
2025-11-27 16:42:29 +0200

    Added UI for register and login

---
+++ client/src/lib/states/commentState.svelte.js
@@ -0,0 +1,45 @@
+import { browser } from "$app/environment"
+import * as commentsApi from "$lib/apis/commentsApi"
+
+let commentState = $state({})
+
+const initComments = async (communityId, postId) => {
+    if (browser) {
+        if (!commentState[communityId]) {
+            commentState[communityId] = {}
+        }
+        commentState[communityId][postId] = await commentsApi.readComments(communityId, postId);
+    }
+}
+
+
+const useCommentState = () => {
+    return {
+        get comments() {
+            return commentState;
+        },
+
+        addComment: async (communityId, postId, comment) => {
+            const newComment = await commentsApi.createComment(communityId, postId, comment);
+
+            if (!commentState[communityId]) commentState[communityId] = {};
+            if (!commentState[communityId][postId]) commentState[communityId][postId] = [];
+
+            commentState[communityId][postId].push(newComment);
+        },
+
+        removeComment: async (communityId, postId, commentId) => {
+            await commentsApi.deleteComment(communityId, postId, commentId);
+
+            if (!commentState[communityId] || !commentState[communityId][postId]) {
+                return
+            }
+
+            commentState[communityId][postId] = commentState[communityId][postId].filter(
+                c => c.id !== commentId
+            );
+        }
+    };
+};
+
+export { initComments, useCommentState };
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<p>Welcome to the home page!</p>
